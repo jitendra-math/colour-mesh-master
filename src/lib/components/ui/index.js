0c7e8742e364e040dd111c1499317977a08cfccd
Jitendra Singh Nimod <i.jitendra.singh0@gmail.com>
2026-02-25 20:45:33 +0530

    Create index.js

---
+++ src/lib/components/ui/index.js
@@ -0,0 +1,5 @@
+export { default as Button } from './Button.svelte';
+export { default as Slider } from './Slider.svelte';
+export { default as Switch } from './Switch.svelte';
+export { default as ColorPip } from './ColorPip.svelte';
+export { default as Tooltip } from './Tooltip.svelte'; // Agar banaya hai to
